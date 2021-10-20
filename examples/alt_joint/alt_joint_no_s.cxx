14565b013cbbb7a52e597fedca98d3dcdd03c574
Ramkumar Natarajan <nrkumar93@gmail.com>
2021-10-20 19:54:06 -0400

    testing with mujoco

--- examples/alt_joint/alt_joint_no_s.cxx
+++ examples/alt_joint/alt_joint_no_s.cxx
@@ -54,18 +54,84 @@
 #include "psopt.h"
 #include <Eigen/Core>
 
-#define DOF 5
+//#define DOF 8
+#define DOF 12
 
 
-adouble qS[] = {0.0, 0.0, 0.0, 0.0, 0.0};
-//adouble qS[] = {0.1,0.1,0.1,0.1,0.1};
-//adouble qS[] = {0.5,0.5,0.5,0.5,0.5};
-adouble qSd[] = {0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qS[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qS[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qS[] = {0.0, 0.0};
+//adouble qSd[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qSd[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qSd[] = {0.0, 0.0};
 //  adouble qG[] = {0.496349540849362,4.29968989868597,5.48318530717959,5.96902604182061,5.88318530717959,6.13318530717959,0.785398163397448};
 //adouble qG[] = {0.496349540849362,4.29968989868597,5.48318530717959,5.96902604182061,5.88318530717959};
-adouble qG[] = {0.1,0.1,0.1,0.1,0.1};
-//adouble qG[] = {0.0, 0.0, 0.0, 0.0, 0.0};
-adouble qGd[] = {0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qG[] = {0.1,0.1,0.1,0.1,0.1,0.1};
+//adouble qG[] = {0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1};
+//adouble qG[] = {0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2};
+//adouble qG[] = {-1.7613, 0.1548, -1.4468, 0.3643, -1.0018, -0.2183};
+//adouble qG[] = {0.1,0.1};
+//adouble qGd[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qGd[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+//adouble qGd[] = {0.0, 0.0};
+
+/// Generic start and goal
+double start = 0.0;
+double dstart = 0.0;
+double goal = 0.2;
+double dgoal = 0.0;
+adouble qS[DOF], qSd[DOF], qG[DOF], qGd[DOF];
+
+//////////////////////////////////////////////////////////////////////////
+///////////////////  Call MJ visualizer (torque) /////////////////////////
+//////////////////////////////////////////////////////////////////////////
+void visualize(Prob& problem, DMatrix t, DMatrix u)
+{
+  int num_nodes = get_number_of_nodes(problem, 1);
+  std::vector<double> vec_t;
+  std::vector<std::vector<double>> vec_u;
+
+  for (int i=0; i<num_nodes; ++i)
+  {
+    std::vector<double> unit_u;
+    for (int j=0; j<DOF; ++j)
+    {
+      unit_u.push_back(u(j,i));
+    }
+    vec_t.push_back(t(i));
+    vec_u.push_back(unit_u);
+  }
+
+  problem.mj_handle.visualize(vec_t.back(), vec_t, vec_u);
+}
+
+//////////////////////////////////////////////////////////////////////////
+///////////////////  Call MJ visualizer (torque) /////////////////////////
+//////////////////////////////////////////////////////////////////////////
+void visualize(Prob& problem, DMatrix t, DMatrix x, DMatrix dx)
+{
+  int num_nodes = get_number_of_nodes(problem, 1);
+  std::vector<double> vec_t;
+  std::vector<std::vector<double>> vec_x;
+  std::vector<std::vector<double>> vec_dx;
+
+  for (int i=0; i<num_nodes; ++i)
+  {
+    std::vector<double> unit_x;
+    std::vector<double> unit_dx;
+    for (int j=0; j<DOF; ++j)
+    {
+      unit_x.push_back(x(j,i));
+      unit_dx.push_back(dx(j,i));
+    }
+    vec_t.push_back(t(i));
+    vec_x.push_back(unit_x);
+    vec_dx.push_back(unit_dx);
+  }
+
+  problem.mj_handle.visualize(vec_t.back(), vec_t, vec_x, vec_dx);
+}
+
 
 //////////////////////////////////////////////////////////////////////////
 ///////////////////  Define the end point (Mayer) cost function //////////
@@ -75,15 +141,15 @@ adouble endpoint_cost(adouble* initial_states, adouble* final_states,
                       adouble* parameters,adouble& t0, adouble& tf,
                       adouble* xad, int iphase, Workspace* workspace)
 {
-//  return tf;
-  double rho = 100.0;
-//  double rho = 1.0;
-  adouble diff[DOF];
-  for (int i=0; i<DOF; ++i)
-  {
-    diff[i] = final_states[2*i] - qG[i];
-  }
-  return rho*dot(diff, diff, DOF);
+  return tf;
+//  double rho = 100.0;
+////  double rho = 1.0;
+//  adouble diff[DOF];
+//  for (int i=0; i<DOF; ++i)
+//  {
+//    diff[i] = final_states[2*i] - qG[i];
+//  }
+//  return rho*dot(diff, diff, DOF);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -97,6 +163,17 @@ adouble integrand_cost(adouble* states, adouble* controls,
   /// Minimize control
   adouble rho = 1.0;
   return rho*dot(controls, controls, DOF);
+
+  /// Minimize acceleration
+//  adouble deriv[2*DOF];
+//  workspace->problem->mj_handle.forwardSimulate(states, controls, deriv);
+//  adouble cost=0;
+//  for (int i=0; i<DOF; ++i)
+//  {
+//    cost += deriv[2*i+1]*deriv[2*i+1];
+//  }
+//  return cost;
+
 
   /// Minimize weighted control
 //  adouble cost = 0;
@@ -155,6 +232,14 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
 
 int main(void) {
 
+  for (int i=0; i<DOF; ++i)
+  {
+    qS[i] = start;
+    qSd[i] = dstart;
+    qG[i] = goal;
+    qGd[i] = dgoal;
+  }
+
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Declare key structures ////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
@@ -188,8 +273,8 @@ int main(void) {
   int ncontrols = DOF;
   int nevents = 4*DOF;
   int npath = 0;
-  int nodes = 40;
-  double t0 = 1.0;
+  int nodes = 25;
+  double t0 = 0.1;
   double tmax = 20.0;
 
 
@@ -200,6 +285,17 @@ int main(void) {
   problem.phases(1).nodes << nodes;
 
   psopt_level2_setup(problem, algorithm);
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////  Setup Mujoco interface ///////////////////////////
+////////////////////////////////////////////////////////////////////////////
+//  const char* mjcf_file = "/home/gaussian/cmu_ri_phd/phd_misc/mujoco200_linux/model/alt_joint/alt_joint.xml";
+//  const char* mjcf_file = "/home/gaussian/cmu_ri_phd/phd_misc/mujoco200_linux/model/alt_joint/pendulum.xml";
+//  const char* mjcf_file = "/home/gaussian/cmu_ri_phd/phd_misc/mujoco200_linux/model/ur5_exp.xml";
+  const char* mjcf_file = "/home/gaussian/cmu_ri_phd/phd_misc/mujoco200_linux/model/ur5l.xml";
+//  const char* mjcf_file = "/home/gaussian/cmu_ri_phd/phd_misc/mujoco200_linux/model/arm_gripper/arm_hand.xml";
+  problem.mj_handle.setupFromMJCFFile(mjcf_file);
+  problem.mj_backend = true;
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -313,15 +409,22 @@ int main(void) {
 
   algorithm.nlp_method                  = "IPOPT";
   algorithm.scaling                     = "automatic";
-  algorithm.derivatives                 = "automatic";
-  algorithm.nlp_iter_max                = 60;
+//  algorithm.derivatives                 = "automatic";
+  algorithm.derivatives                 = "numerical";
+  algorithm.nlp_iter_max                = 200;
+//  algorithm.nlp_tolerance               = 1.e-4;
   algorithm.nlp_tolerance               = 1.e-2;
+//  algorithm.ipopt_linear_solver         = "ma27";
+  algorithm.print_level                 = 1;
+  algorithm.collocation_method          = "Hermite-Simpson";
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Now call PSOPT to solve the problem   /////////////////
 ////////////////////////////////////////////////////////////////////////////
 
   psopt(solution, problem, algorithm);
+
+  std::cout << "the collocation method is: " << algorithm.collocation_method << std::endl;
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////  Extract relevant variables from solution structure   //////////
@@ -346,14 +449,23 @@ int main(void) {
 
 //  plot(t,x,problem.name + ": states", "time (s)", "states", "x1 x2 x3 x4 x5 x6 x7 x8 x9 x10");
   DMatrix z = zeros(DOF, nodes);
+  DMatrix dz = zeros(DOF, nodes);
   for (int i=0; i<DOF; ++i)
   {
     z.block(i, 0, 1, nodes) = x.block(2*i, 0, 1, nodes);
+    dz.block(i, 0, 1, nodes) = x.block(2*i+1, 0, 1, nodes);
   }
-  plot(t,z,problem.name + ": states", "time (s)", "states", "x0 x2 x4 x6 x8");
+//  plot(t,z,problem.name + ": states", "time (s)", "states", "x0 x2 x4 x6 x8 x10 x12 x14");
+//  plot(t,dz,problem.name + ": velocities", "time (s)", "states", "x1 x3 x5 x7 x9 x11 x13 x15");
+//  plot(t,u,problem.name + ": controls", "time (s)", "controls", "u1 u2 u3 u4 u5 u6 u7 u8");
 
-  plot(t,u,problem.name + ": controls", "time (s)", "controls", "u1 u2 u3 u4 u5");
+//  plot(t,z,problem.name + ": states", "time (s)", "states", "x0 x2");
+//  plot(t,dz,problem.name + ": velocities", "time (s)", "states", "x1 x3");
+//  plot(t,u,problem.name + ": controls", "time (s)", "controls", "u1 u2");
 
+  plot(t,z,problem.name + ": states", "time (s)", "states");
+  plot(t,dz,problem.name + ": velocities", "time (s)", "states");
+  plot(t,u,problem.name + ": controls", "time (s)", "controls");
 
 //  plot(t,x,problem.name + ": states", "time (s)", "states", "x1 x2 x3 x4 x5 x6 x7 x8 x9 x10",
 //       "pdf", "alt_joint_states.pdf");
@@ -361,12 +473,20 @@ int main(void) {
 //  plot(t,u,problem.name + ": controls", "time (s)", "controls", "u1 u2 u3 u4 u5",
 //       "pdf", "alt_joint_controls.pdf");
 
-  std::cout << "+++++++++++++++++++++++ with init guess ++++++++++++++++++++++++++" << std::endl;
+//  std::cout << "+++++++++++++++++++++++ with init guess ++++++++++++++++++++++++++" << std::endl;
+//
+//  problem.phases(1).guess.controls       = u;
+//  problem.phases(1).guess.states         = x;
+//  problem.phases(1).guess.time           = t;
+//  psopt(solution, problem, algorithm);
 
-  problem.phases(1).guess.controls       = u;
-  problem.phases(1).guess.states         = x;
-  problem.phases(1).guess.time           = t;
-  psopt(solution, problem, algorithm);
+
+///// Call Visualizer
+//  const char* mjcf_pb_file = "/home/gaussian/cmu_ri_phd/phd_misc/mujoco200_linux/model/ur5_pb.xml";
+//  problem.mj_handle.setupFromMJCFFile(mjcf_pb_file);
+
+  //  visualize(problem, t, u);
+  visualize(problem, t, z, dz);
 
 }
 
